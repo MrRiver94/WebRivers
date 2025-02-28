fd887c054c482ed32347cd81afadd6c5194a5d9a
unknown <danielriosproduccion@gmail.com>
2025-02-28 14:03:32 +0100

    Reconfi

--- src/containers/Navbar/Navbar.jsx
+++ src/containers/Navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 import './Navbar.css';
 
 function Navbar() {
